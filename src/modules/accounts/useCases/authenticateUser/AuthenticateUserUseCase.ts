399233cad1f9686fe9014f85cab7c7d0c567611e
gilmarferrini <ferrinigilmar@gmail.com>
2021-10-02 09:16:46 -0300

    adicionado useCase de autenticacao

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,53 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseUse {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Email or password incorrect!");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email or password incorrect!");
+    }
+
+    const token = sign({}, "5e7*7C18e2082d&1781cfdbecb8addc3", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export { AuthenticateUserUseUse };
