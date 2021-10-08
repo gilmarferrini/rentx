9e70a1f5f80ee03390cc5d64240008d4e200c539
gilmarferrini <ferrinigilmar@gmail.com>
2021-10-08 19:38:55 -0300

    refatorando estrutura do projeto

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { ensureAuthenticated } from "middlewares/ensureAuthenticated";
 import multer from "multer";
 
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
+import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
-import uploadConfig from "../config/upload";
+import uploadConfig from "../../../../config/upload";
 
 const usersRoutes = Router();
 
