0052e0504bb0661d7f961cb8a30bc80d5cea222f
gilmarferrini <ferrinigilmar@gmail.com>
2021-10-12 09:32:22 -0300

    adicionado middleware para verificar se e admin

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -2,9 +2,17 @@ import { Router } from "express";
 
 import { AuthenticateController } from "@modules/accounts/useCases/authenticateUser/AuthenticateController";
 
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+
 const authenticateRoutes = Router();
 const authenticateController = new AuthenticateController();
 
-authenticateRoutes.post("/sessions", authenticateController.handle);
+authenticateRoutes.post(
+  "/sessions",
+  ensureAuthenticated,
+  ensureAdmin,
+  authenticateController.handle
+);
 
 export { authenticateRoutes };
--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -5,6 +5,9 @@ import { CreateCategoryController } from "@modules/cars/useCases/createCategory/
 import { ImportCategoryController } from "@modules/cars/useCases/importCategory/ImportCategoryController";
 import { ListCategoriesController } from "@modules/cars/useCases/listCategories/ListCategoriesController";
 
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+
 const categoriesRoutes = Router();
 
 const upload = multer({
@@ -22,6 +25,8 @@ categoriesRoutes.get("/", listCategoriesController.handle);
 categoriesRoutes.post(
   "/import",
   upload.single("file"),
+  ensureAuthenticated,
+  ensureAdmin,
   importCategoryController.handle
 );
 
