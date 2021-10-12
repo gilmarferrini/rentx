08a33bae4302faa42aba5509142b5527cfa278ac
gilmarferrini <ferrinigilmar@gmail.com>
2021-10-12 19:43:29 -0300

    terminando a logica da listagem de carros disponiveis

--- src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.spec.ts
+++ src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.spec.ts
@@ -1,14 +1,14 @@
 import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
 
-import { ListCarsUseCase } from "./ListCarsUseCase";
+import { ListAvailableCarsUseCase } from "./ListAvailableCarsUseCase";
 
 let carsRepositoryInMemory: CarsRepositoryInMemory;
-let listCarsUseCase: ListCarsUseCase;
+let listCarsUseCase: ListAvailableCarsUseCase;
 
 describe("List Cars", () => {
   beforeEach(() => {
     carsRepositoryInMemory = new CarsRepositoryInMemory();
-    listCarsUseCase = new ListCarsUseCase(carsRepositoryInMemory);
+    listCarsUseCase = new ListAvailableCarsUseCase(carsRepositoryInMemory);
   });
 
   it("should be able to list all available cars", async () => {
@@ -40,6 +40,42 @@ describe("List Cars", () => {
 
     const cars = await listCarsUseCase.execute({
       name: "Car2",
+    });
+
+    expect(cars).toEqual([car]);
+  });
+
+  it("should be able to list all available cars by brand", async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: "Car3",
+      description: "Car description",
+      daily_rate: 110.0,
+      license_plate: "DEF-1235",
+      fine_amount: 40,
+      brand: "Car_brand_Test",
+      category_id: "category_id",
+    });
+
+    const cars = await listCarsUseCase.execute({
+      brand: "Car_brand_test",
+    });
+
+    expect(cars).toEqual([car]);
+  });
+
+  it("should be able to list all available cars by category", async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: "Car4",
+      description: "Car description",
+      daily_rate: 110.0,
+      license_plate: "DEF-1237",
+      fine_amount: 40,
+      brand: "Car_brand_Test",
+      category_id: "12345",
+    });
+
+    const cars = await listCarsUseCase.execute({
+      category_id: "12345",
     });
 
     expect(cars).toEqual([car]);
