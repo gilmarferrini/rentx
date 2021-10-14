2aa5819e0d33533e47de6f12aacaddad8b4d9ba5
gilmarferrini <ferrinigilmar@gmail.com>
2021-10-14 18:14:46 -0300

    adicionado teste para retorno do carro caso ele existir

--- src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
@@ -25,4 +25,23 @@ describe("Create Car Specification", () => {
       });
     }).rejects.toBeInstanceOf(AppError);
   });
+
+  it("should be able to add a new specification to the car", async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: "Name Car",
+      description: "Description Car",
+      daily_rate: 100,
+      license_plate: "ABC-1234",
+      fine_amount: 60,
+      brand: "Brand",
+      category_id: "category",
+    });
+
+    const specifications_id = ["5432"];
+
+    await createCarSpecificationUseCase.execute({
+      car_id: car.id,
+      specifications_id,
+    });
+  });
 });
