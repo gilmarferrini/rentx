b0ad29bdd8b3c66ebbb1660b5866e59caf720028
gilmarferrini <ferrinigilmar@gmail.com>
2021-10-21 18:19:22 -0300

    adicionado metodo create no repositorio de alugueis em memoria

--- src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
+++ src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -1,9 +1,28 @@
+import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 
 import { IRentalsRepository } from "../IRentalsRepository";
 
 class RentalsRepositoryInMemory implements IRentalsRepository {
   rentals: Rental[] = [];
+
+  async create({
+    user_id,
+    car_id,
+    expected_return_date,
+  }: ICreateRentalDTO): Promise<Rental> {
+    const rental = new Rental();
+
+    Object.assign(rental, {
+      car_id,
+      user_id,
+      expected_return_date,
+    });
+
+    this.rentals.push(rental);
+
+    return rental;
+  }
 
   async findOpenRentalByCar(car_id: string): Promise<Rental> {
     return this.rentals.find(
