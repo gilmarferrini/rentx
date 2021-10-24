3a8bd00e172fb4412f3ba155df6f7c576b6ca443
gilmarferrini <ferrinigilmar@gmail.com>
2021-10-24 09:12:17 -0300

    registrando singleton de rentals

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -10,6 +10,8 @@ import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRep
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
 import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
+import { RentalsRepository } from "@modules/rentals/infra/typeorm/repositories/RentalsRepository";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
@@ -36,4 +38,9 @@ container.registerSingleton<ISpecificationsRepository>(
 container.registerSingleton<ICarsImagesRepository>(
   "CarsImageRepository",
   CarsImagesRepository
+);
+
+container.registerSingleton<IRentalsRepository>(
+  "RentalsRepository",
+  RentalsRepository
 );
