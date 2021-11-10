d03657ecff659cdd71c79dbf2bfd6349130dbd71
gilmarferrini <ferrinigilmar@gmail.com>
2021-11-10 19:24:51 -0300

    adicionado arquivo para salvar dados de configuracao de autenticacao

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "5e7*7C18e2082d&1781cfdbecb8addc3",
+  expires_in_token: "15m",
+  secret_refresh_token: "1434klk;lhjgk321321",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
