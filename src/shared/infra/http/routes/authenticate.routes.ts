d0419e053dc67f7081f0e6d02c977e49d7b0615d
gilmarferrini <ferrinigilmar@gmail.com>
2021-11-14 17:15:43 -0300

    criando controller refresh token

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 
 import { AuthenticateController } from "@modules/accounts/useCases/authenticateUser/AuthenticateController";
-
-import { ensureAdmin } from "../middlewares/ensureAdmin";
-import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { RefreshTokenController } from "@modules/accounts/useCases/refreshToken/RefreshTokenController";
 
 const authenticateRoutes = Router();
+
 const authenticateController = new AuthenticateController();
+const refreshTokenController = new RefreshTokenController();
 
 authenticateRoutes.post("/sessions", authenticateController.handle);
+authenticateRoutes.post("/refresh-token", refreshTokenController.handle);
 
 export { authenticateRoutes };
